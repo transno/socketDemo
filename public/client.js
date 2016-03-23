40d1fcf342a897482347322d829631c35e47123c
JohnnyR <ymwdleifeng@163.com>
2016-03-23 22:17:51 +0800

    添加头像

--- public/client.js
+++ public/client.js
@@ -2,12 +2,18 @@ var Chatroom = {
     username: null,
     userid: null,
     socket: null,
+    picurl: 1,
     userNameSubmit: function() {
         var username = $('#userNameInput').val();
         if (username == '') {
-            alert('请输入用户名！');
+            alert('少侠，行走江湖来个响亮的名头吧！');
             return;
         }
+        if (username.length > 10) {
+            alert('少侠，哪国人啊，名头太响了不能在这混喔！');
+            return;
+        }
+
         $('.loginCon').hide();
         this.init(username);
         return false;
@@ -21,15 +27,20 @@ var Chatroom = {
             var message = {
                 userid: this.userid,
                 username: this.username,
-                msg: msg
+                msg: msg,
+                avatar: this.picurl
             }
             this.socket.emit('clinet_chat_message', message);
         }
         return false;
     },
+    getRandom: function() {
+        return Math.ceil(Math.random() * 30);
+    },
     init: function(username) {
         this.userid = this.giveUid();
         this.username = username;
+        this.picurl = this.getRandom();
         self.myHtml = this.myHtml();
         self.userid = this.userid;
         this.socket = io();
@@ -40,44 +51,50 @@ var Chatroom = {
         // 接收登录信息
         this.socket.on('logined', function(msg) {
             // console.log(msg);
-            $('#chatCon').append('<div class="welcomeCon"><span>欢迎<b>' + msg.user.username + '</b>加入</span></div>');
+            $('#chatCon').append('<div class="welcomeCon"><span><b>' + msg.user.username + '</b>初入江湖！</span></div>');
             $('#onlineNum').html('在线用户：' + msg.onlineNum + '人');
+            var dh = $(document).height();
+            $('body').animate({ scrollTop: dh }, 500);
         });
 
         // 接收退出信息
         this.socket.on('logout', function(msg) {
-            $('#chatCon').append('<div class="welcomeCon"><span><b>' + msg.user + '</b>退出！</span></div>');
+            $('#chatCon').append('<div class="welcomeCon"><span><b>' + msg.user + '</b>淡出江湖！</span></div>');
             $('#onlineNum').html('在线用户：' + msg.onlineNum + '人');
+            var dh = $(document).height();
+            $('body').animate({ scrollTop: dh }, 500);
         });
 
         // 接收聊天信息
         this.socket.on('server_chat_message', function(msg) {
-            var myMessages = Chatroom.myHtml(msg.msg, msg.username);
-            var otherMessages = Chatroom.otherHtml(msg.msg, msg.username);
+            var myMessages = Chatroom.myHtml(msg.msg, msg.username, msg.avatar);
+            var otherMessages = Chatroom.otherHtml(msg.msg, msg.username, msg.avatar);
             if (msg.userid == self.userid) {
                 $('#chatCon').append(myMessages);
             } else {
                 $('#chatCon').append(otherMessages);
             }
+            var dh = $(document).height();
+            $('body').animate({ scrollTop: dh }, 500);
         });
     },
-    myHtml: function(msg, username) {
+    myHtml: function(msg, username, url) {
         var myHtml = '<div class="messageCon my">' +
-            			'<div class="username">' + username + '</div>' +
-            			'<div class="clear">' +
-            				'<div class="img"></div>' +
-            				'<div class="messageBox clear">' +
-            					'<div class="info">' + msg + '</div>' +
-            				'</div>' +
-            			'</div>' +
-            		'</div>';
+            '<div class="username">' + username + '</div>' +
+            '<div class="clear">' +
+            '<div class="img" style="background-image:url(avatar/' + url + '.jpg)"></div>' +
+            '<div class="messageBox clear">' +
+            '<div class="info">' + msg + '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
         return myHtml;
     },
-    otherHtml: function(msg, username) {
+    otherHtml: function(msg, username, url) {
         var myHtml = '<div class="messageCon other">' +
             '<div class="username">' + username + '</div>' +
             '<div class="clear">' +
-            '<div class="img"></div>' +
+            '<div class="img" style="background-image:url(avatar/' + url + '.jpg)"></div>' +
             '<div class="messageBox clear">' +
             '<div class="info">' + msg + '</div>' +
             '</div>' +
@@ -96,4 +113,11 @@ $('form').submit(function() {
 $(function() {
     var h = $(window).height();
     $('body').css('min-height', h);
+
+    $('#userNameInput').keydown(function(event) {
+    	// console.log(event);
+    	if (event.keyCode == 13) {
+    		Chatroom.userNameSubmit();
+    	}
+    });
 })
